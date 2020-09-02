3161c5ac459f76252c416aa976b988096fd4312f
Mike <mikedejong4996@gmail.com>
2020-09-02 15:52:07 +0200

    Initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>{{config('app.name', 'Laravel-Webshop')}}</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    </head>
+    <body>
+        <!-- navbar -->
+        @include('inc.navbar')
+
+        <!-- page content -->
+        <div class="container">
+            @yield('content')
+        </div>
+
+        <!-- footer -->
+
+    </body>
+</html>
